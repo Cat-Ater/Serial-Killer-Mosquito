c195b16adfcaa6b972575587565daed54ac3105d
Cat Ater <Cheshyrecat1337@gmail.com>
2024-04-05 13:47:02 +1100

    Added Game_SFX Component to game for audio addressing.

--- SK Mozzie/Assets/Scripts/UI/Dialogue/UIDialogueData.cs
+++ SK Mozzie/Assets/Scripts/UI/Dialogue/UIDialogueData.cs
@@ -9,14 +9,29 @@ namespace UI
     [Serializable]
     public struct UIDialogueData
     {
+        /// <summary>
+        /// The lines contained within the dialouge. 
+        /// </summary>
         [Header("The lines of text to display. ")]
         public UIDialogueLine[] lines;
+        /// <summary>
+        /// The audio clip to play on opening the text box. 
+        /// </summary>
         [Header("The AudioClip to play on opening the dialogue box. ")]
         public AudioClip textOpenSFX;
+        /// <summary>
+        /// The audio clip to play on closing the text box. 
+        /// </summary>
         [Header("The AudioClip to play on closing the dialogue box. ")]
         public AudioClip textCloseSFX;
+        /// <summary>
+        /// The audio clip to play on each character being typed. 
+        /// </summary>
         [Header("The AudioClip to play while writing out each character. ")]
         public AudioClip textTypeSFX;
+        /// <summary>
+        /// The audio clip to play on skipping a dialogue line. 
+        /// </summary>
         [Header("The AudioClip to play on skipping to the end of the dialogue line. ")]
         public AudioClip textSkipSFX;
 
