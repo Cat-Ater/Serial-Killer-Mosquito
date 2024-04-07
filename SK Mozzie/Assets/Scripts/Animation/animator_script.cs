002636e7e44e35bff4555551035923ada648aff3
Takainex <kathrinsteppat@gmail.com>
2024-04-07 16:44:37 +1000

    added dog anim

--- SK Mozzie/Assets/Scripts/Animation/animator_script.cs
+++ SK Mozzie/Assets/Scripts/Animation/animator_script.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class animator_script : MonoBehaviour
+public class TargetAnimatorController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TargetData targetData;
+    public Animator animator;
+
+    private void Start()
     {
-        
+        // Make sure targetData and animator are assigned
+        if (targetData == null || animator == null)
+        {
+            Debug.LogError("TargetData or Animator is not assigned in TargetAnimatorController.");
+            return;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check the state of the target and set animator bools accordingly
+        switch (targetData.targetHealth.state)
+        {
+            case TargetHealthState.ALIVE:
+                SetAnimatorBools(targetData.playIdleAnim, false, false);
+                break;
+            case TargetHealthState.ATTACKED:
+                SetAnimatorBools(false, targetData.playAttackAnim, false);
+                break;
+            case TargetHealthState.DEAD:
+                SetAnimatorBools(false, false, targetData.playDeathAnim);
+                break;
+        }
+    }
+
+    // Method to set animator bools
+    private void SetAnimatorBools(bool idle, bool attacked, bool dead)
+    {
+        animator.SetBool("Idle", idle);
+        animator.SetBool("Attacked", attacked);
+        animator.SetBool("Dead", dead);
     }
 }
+
